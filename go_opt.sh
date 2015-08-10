ee64ac94969514f12a3b4f43f2e4e343e188f0ff
Taylor Ellis <s3282813@csitprdap02.int.its.rmit.edu.au>
2015-08-10 18:29:14 +1000

    Version 2.1

--- go_opt.sh
+++ go_opt.sh
@@ -5,6 +5,17 @@ case $opt in
 	a)
 	echo "-a was triggered!" >&2
 	;;
+	b)
+	echo "-b was triggered!" >&2
+	;;
+	c)
+	echo "-c was triggered" >&2
+	echo $OPTARG
+	;;
+	d)
+	echo "-d was triggered" >&2
+	echo $OPTARG
+	;;
 	\?)
 	echo "Invalid option: -$OPTARG" >&2
 	;;
